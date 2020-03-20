357c5245dc38cb60eed92618aa0384ee93220ba1
waititu <waititurobbin@gmail.com>
2020-03-20 15:25:14 +0300

    add crispy-template-pack

--- cryptosavings/cryptosavings/settings.py
+++ cryptosavings/cryptosavings/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'savings.apps.SavingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
