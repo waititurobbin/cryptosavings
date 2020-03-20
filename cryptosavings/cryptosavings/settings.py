5c226039d3d4ae39a8ea7102f9d562d287162cdf
waititu <waititurobbin@gmail.com>
2020-03-20 14:22:36 +0300

    add a registration template

--- cryptosavings/cryptosavings/settings.py
+++ cryptosavings/cryptosavings/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'savings.apps.SavingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
