97ff5510618a17e4b940e14ce3d504885c9ed5a1
waititu <waititurobbin@gmail.com>
2020-03-20 20:12:04 +0300

    add a logout template

--- cryptosavings/savings/urls.py
+++ cryptosavings/savings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='savings-home'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('about/', views.about_view, name='savings-about'),
     path('contacts/', views.contacts_view, name='savings-contacts'),
 ]
