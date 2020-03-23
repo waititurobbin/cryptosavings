9f58908b106c3104dc44f229eda1a2acf1390332
waititu <waititurobbin@gmail.com>
2020-03-23 22:23:00 +0300

    polish password reset feauture

--- cryptosavings/cryptosavings/settings.py
+++ cryptosavings/cryptosavings/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('DB_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
+EMAIL_HOST_USER = 'waititurobbin@gmail.com'
+EMAIL_HOST_PASSWORD = 'zxpmmknsvexfnbog'
